70a364a33aa3493becef149fc38b954131284bd7
juanjohn <juanjhn@gmail.com>
2018-03-19 10:23:36 +0530

    dp

---
+++ dp/rod_cutting.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+ll inp(){
+	register ll c = getchar_unlocked();
+	ll neg=0;
+	ll ret=0;
+	for(; ((c<48 || c>57) && c!='-'); c = getchar_unlocked());
+	if(c=='-') {
+    	neg=1;c=getchar_unlocked();
+    }
+	for(; c>47 && c<58 ; c = getchar_unlocked()) {
+		ret = 10 * ret + c - 48;
+	}
+	if(neg)
+		ret=-ret;
+	return ret;
+}
+
+int main(){
+	ll i,j,k,t,sum,res,cnt,a,b,c,arr[100000],q,l,r,n;
+	t=inp();
+
+	while(t--){
+		l=inp();
+
+	}
+	return 0;
+}
